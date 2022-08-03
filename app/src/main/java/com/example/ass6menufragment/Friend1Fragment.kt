d01a86b7ff652e17e7d3123b2b29d91da7120159
monlica <wattana.mcs@gmail.com>
2022-08-03 23:36:53 +0700

    8-3-2556

--- app/src/main/java/com/example/ass6menufragment/Friend1Fragment.kt
+++ app/src/main/java/com/example/ass6menufragment/Friend1Fragment.kt
@@ -5,17 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ass6menufragment.databinding.ActivityMainBinding
+import com.example.ass6menufragment.databinding.FragmentFriend1Binding
+import com.example.ass6menufragment.databinding.FragmentFriend2Binding
 
 
 class Friend1Fragment : Fragment() {
-
+    private lateinit var bindingFrag: FragmentFriend1Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend1, container, false)
+        bindingFrag = FragmentFriend1Binding.inflate(layoutInflater)
+        bindingFrag.btnBack.setOnClickListener(){
+            var fragment : Fragment? = null
+            fragment = MyFragment()
+            replaceFragment(fragment)
+        }
+        return bindingFrag.root
     }
 
+    fun replaceFragment(someFragment:Fragment){
+        var binding: ActivityMainBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        //transaction.replace(R.id.frameLayout, someFragment)
+        transaction.replace(binding.frameLayout.id, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 
 }
--- app/src/main/java/com/example/ass6menufragment/MainActivity.kt
+++ app/src/main/java/com/example/ass6menufragment/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ass6menufragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,31 @@ class MainActivity : AppCompatActivity() {
                 R.id.frameLayout,
                 MyFragment()
             ).commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Get the support fragment manager instance
+        val manager = supportFragmentManager
+        // Begin the fragment transition using support fragment manager
+        val transaction = manager.beginTransaction()
+        when (item?.itemId) {
+            R.id.menu1 -> {
+                transaction.replace(R.id.frameLayout,Friend1Fragment())
+                transaction.addToBackStack(null)
+                transaction.commit()
+                return true}
+            R.id.menu2 -> {transaction.replace(R.id.frameLayout,Friend2Fragment())
+                transaction.addToBackStack(null)
+                transaction.commit()
+                return true}
+
+            else ->  return super.onOptionsItemSelected(item)
         }
     }
 }
